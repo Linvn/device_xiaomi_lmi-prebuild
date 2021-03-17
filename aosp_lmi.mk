78c2fda5b5809ff3a90cb250a1d23b3c7d369462
TheDoctor12 <jkaskabas95@gmail.com>
2021-03-17 08:51:43 +0200

    lmi: Update fingerprint and build description to V12.2.5.0.RJKMIXM
    Also change product model to match MIUI

--- aosp_lmi.mk
+++ aosp_lmi.mk
@@ -3,15 +3,28 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-
+# Inherit from lmi device
 $(call inherit-product, device/xiaomi/lmi/device.mk)
 
-# Inherit some common aicp stuff.
+# Inherit some common PixelExperience stuff.
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_STOCK_ARCORE := true
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_lmi
 PRODUCT_DEVICE := lmi
 PRODUCT_BRAND := POCO
-PRODUCT_MODEL := F2 Pro
+PRODUCT_MODEL := POCO F2 Pro
 PRODUCT_MANUFACTURER := Xiaomi
+
+BUILD_FINGERPRINT := "Redmi/lmi/lmi:11/RKQ1.200826.002/V12.2.5.0.RJKMIXM:user/release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="lmi-user 11 RKQ1.200826.002 V12.2.5.0.RJKMIXM release-keys" \
+    PRODUCT_NAME="lmi" \
+    TARGET_DEVICE="lmi"
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
