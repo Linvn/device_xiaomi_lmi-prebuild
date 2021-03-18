f9974228015015870982799cc8e0028958cb94ea
lin <llin1079@gmail.com>
2021-03-18 02:04:33 +0000

    lmi:fix qccsyshal

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -89,6 +89,7 @@ TARGET_USES_FOD_ZPOS := true
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_lmi
